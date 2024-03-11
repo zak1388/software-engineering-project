18fcf40051206f893796d5583c01b5079c1f4fad
saifk <ec22616@qmul.ac.uk>
2024-03-11 14:15:50 +0000

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require("express")
 const cors = require("cors")
+const mongoose = require("mongoose")
 
 const app = express()
+
+mongoose.connect("mongodb+srv://zak:ECS506@cluster0.3ranwb2.mongodb.net/", {
+
+}).then((response) => {
+    if(response){
+        console.log("db connected")
+    }
+})
 
 app.use(cors())
 
