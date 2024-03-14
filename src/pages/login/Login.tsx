f24225b83dce4af257cacb70e6209bf358603590
Zakariyya Kadri <zakariyyakadri@gmail.com>
2024-03-14 11:37:23 +0000

    Added login page, still needs header and working on backend

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import styles from "./login.module.css";
+
+// TODO: add header as component
+// TODO: custom fonts
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className={styles.background}>
+        <form className={styles.loginForm}>
+            <div className={styles.field}>
+                <label className={styles.label} for="username">Username: </label>
+                <input className={styles.input} type="text" id="username" name="username" />
+            </div>
+            <div className={styles.field}>
+                <label className={styles.label} for="passowrd">Password: </label>
+                <input className={styles.input} type="password" id="passowrd" name="passowrd" />
+            </div>
+            <input className={styles.submit} type="submit" value="Login" />
+        </form>
+    </div>
   )
 }
 
