3370b4c18e70b318f99c4393593437c7eb38653c
ec22741 <ec22741@qmul.ac.uk>
2024-03-26 11:14:13 +0000

    fixed naming convention + days off flex box

--- src/pages/dashboard/home/Home.tsx
+++ src/pages/dashboard/home/Home.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import styles from "./home.module.css"
 import { HiMiniAdjustmentsHorizontal } from "react-icons/hi2";
+import { FaRegCalendarMinus } from "react-icons/fa";
+import { GiIsland } from "react-icons/gi";
+import { PiFirstAidKitBold } from "react-icons/pi";
 
 function Home() {
   return (
     <div className={styles.container}>
 
 
-        <div className={styles.widgetsGrid}>
+        <div className={styles.widgets_grid}>
             {/* Company Updates Widget */}
             <div className={styles.company_updates}>
-                <div className={styles.updatesHeader}>
+                <div className={styles.updates_header}>
                     <h2>Company Updates</h2>
-                    <h5 className={styles.newUpdates}>2 new</h5>
+                    <h5 className={styles.new_updates}>2 new</h5>
                 </div>
                 <p className={styles.date}>25 March</p>
-                <p className={styles.updateBlock}>Example Update</p>
-                <p className={styles.updateBlock}>Another Update</p>
+                <p className={styles.update_block}>Example Update</p>
+                <p className={styles.update_block}>Another Update</p>
             </div>
 
             {/* Next Leave Widget */}
@@ -41,9 +44,9 @@ function Home() {
 
             {/* Team Updates Widget */}
             <div className={styles.team_updates}>
-                <div className={styles.updatesHeader}>
+                <div className={styles.updates_header}>
                     <h2>Team Updates</h2>
-                    <h5 className={styles.newUpdates}>1 new</h5>
+                    <h5 className={styles.new_updates}>1 new</h5>
                 </div>
             </div>
 
@@ -51,16 +54,19 @@ function Home() {
             <div className={styles.days_off}>
                 {/* Days Off */}
                 <div className={styles.balance_block}>
+                    <FaRegCalendarMinus className={styles.days_off_icon}/>
                     <h2>Days Off</h2>
                 </div>
 
                 {/* Holiday */}
                 <div className={styles.balance_block}>
+                    <GiIsland className={styles.days_off_icon}/>
                     <h2>Holiday</h2>
                 </div>
 
                 {/* Sick Days */}
                 <div className={styles.balance_block}>
+                    <PiFirstAidKitBold className={styles.days_off_icon}/>
                     <h2>Sick</h2>
                 </div>
             </div>
