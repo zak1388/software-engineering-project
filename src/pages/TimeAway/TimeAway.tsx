bed5b9fef516f93e088d8ca5f1f3b5cbef1456b3
Zakariyya Kadri <zakariyyakadri@gmail.com>
2024-03-30 23:28:24 +0000

    Request is now a JSX element

--- src/pages/TimeAway/TimeAway.tsx
+++ src/pages/TimeAway/TimeAway.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from "axios";
 import styles from "./TimeAway.module.css"
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../../components/sidebar/Sidebar.tsx'
@@ -8,6 +9,17 @@ import { CiCircleCheck } from "react-icons/ci";
 import { CiCircleRemove } from "react-icons/ci";
 
 function TimeAway () {
+    const [leaves, setLeaves] = useState();
+
+    useEffect(() => {
+        Axios.post("http://localhost:8000/api/GetLeaveRequests", {
+            params: { userId: localStorage.getItem("userId") }
+        }).then(response => {
+            console.log("Got leave requests as:", response);
+            setLeaves(response);
+        }, (err) => console.log("Failed to get leave reqs", err));
+    }, []);
+
     return (
         <div className={styles.container}>
             <div className={styles.NewButton}>
@@ -19,81 +31,79 @@ function TimeAway () {
             </div>
             <div className={styles.HolidayInfo}>
                 <div className={styles.titles}>
-                    <h2 className={styles.title0}>Type</h2>
-                    <h2 className={styles.title1}>Grant (Days)</h2>
-                    <h2 className={styles.title1}>Approved</h2>
-                    <h2 className={styles.title1}>Remaining Days</h2>
+                    <h2 className={styles.title}>Type</h2>
+                    <h2 className={styles.title}>Grant (Days)</h2>
+                    <h2 className={styles.title}>Approved</h2>
+                    <h2 className={styles.title}>Remaining Days</h2>
                 </div>
-                <div className={styles.dataset1}>
-                    <h1 className={styles.data0}>Holiday</h1>
-                    <h1 className={styles.data1}>25.0</h1>
-                    <h1 className={styles.data2}>2.0</h1>
-                    <h1 className={styles.data3}>23.0</h1>
+                <div className={styles.dataset}>
+                    <h1 className={styles.data}>Holiday</h1>
+                    <h1 className={styles.data}>25.0</h1>
+                    <h1 className={styles.data}>2.0</h1>
+                    <h1 className={styles.data}>23.0</h1>
                 </div>
-                <div className={styles.dataset2}>
-                    <h1 className={styles.data4}>Holiday Carrying Over</h1>
-                    <h1 className={styles.data5}>0.0</h1>
-                    <h1 className={styles.data6}>0.0</h1>
-                    <h1 className={styles.data7}>0.0</h1>
+                <div className={styles.dataset}>
+                    <h1 className={styles.data}>Holiday Carrying Over</h1>
+                    <h1 className={styles.data}>0.0</h1>
+                    <h1 className={styles.data}>0.0</h1>
+                    <h1 className={styles.data}>0.0</h1>
                 </div>
-                <div className={styles.dataset3}>
-                    <h1 className={styles.data8}>Sick Leave</h1>
-                    <h1 className={styles.data9}>N/A</h1>
-                    <h1 className={styles.data10}>1.0</h1>
-                    <h1 className={styles.data11}>N/A</h1>
-                </div>
-                
-            </div>
-            <div className={styles.request2}>
-                <div>
-                    <h3>Type</h3>
-                    <h1>Holiday</h1>
-                </div>
-                <div className={styles.StartDate}>
-                    <h3>Start Date</h3>
-                    <h1>Tuesday 19th March 2024</h1>
-                </div>
-
-                <div className={styles.EndDate}>
-                    <h3>End Date</h3>
-                    <h1>Tuesday 19th March 2024</h1>
-                </div>
-
-                <div className={styles.Accept}>
-                    <h1>
-                        <span>
-                            <CiCircleCheck className={styles.Tick}/>
-                        </span>
-                        Approved
-                    </h1>
+                <div className={styles.dataset}>
+                    <h1 className={styles.data}>Sick Leave</h1>
+                    <h1 className={styles.data}>N/A</h1>
+                    <h1 className={styles.data}>1.0</h1>
+                    <h1 className={styles.data}>N/A</h1>
                 </div>
             </div>
-            <div className={styles.request3}>
-                <div>
-                    <h3>Type</h3>
-                    <h1>Holiday</h1>
-                </div>
-                <div className={styles.StartDate}>
-                    <h3>Start Date</h3>
-                    <h1>Tuesday 19th March 2024</h1>
-                </div>
-
-                <div className={styles.EndDate}>
-                    <h3>End Date</h3>
-                    <h1>Tuesday 19th March 2024</h1>
-                </div>
-
-                <div className={styles.Reject}>
-                    <h1>
-                        <span>
-                            <CiCircleRemove className={styles.cross}/>
-                        </span>
-                        Rejected
-                    </h1>
-                </div>
-            </div>
+            <Request 
+                type="Holiday"
+                start_date="Tuesday 19th March 2024"
+                end_date="Tuesday 19th March 2024"
+                accepted="true"
+            />
+            <Request 
+                type="Holiday"
+                start_date="Tuesday 29th March 2024"
+                end_date="Tuesday 29th March 2024"
+                accepted="{false}"
+            />
         </div>
     )
+}
+
+function Request({type, start_date, end_date, accepted}) {
+    return (
+            <div className={styles.request}>
+                <div>
+                    <h3>Type</h3>
+                    <h1>{type}</h1>
+                </div>
+                <div className={styles.StartDate}>
+                    <h3>Start Date</h3>
+                    <h1>{start_date}</h1>
+                </div>
+
+                <div className={styles.EndDate}>
+                    <h3>End Date</h3>
+                    <h1>{end_date}</h1>
+                </div>
+
+                <div className={styles.Accepted}>
+                    <span>
+                        {
+                            accepted && <CiCircleCheck className={styles.Tick}/> 
+                            || <CiCircleRemove className={styles.cross}/>
+                        }
+                    </span>
+                    <h1>
+                        {
+                            accepted && "Accepted"
+                            || "Rejected"
+                        }
+                    </h1>
+                </div>
+            </div>
+    );
 }
 
 export default TimeAway
