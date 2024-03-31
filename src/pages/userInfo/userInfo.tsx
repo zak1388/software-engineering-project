ce7f2557f7aa16af7e6a8f4e7783e87932472bb0
Benjamin Camilleri <camilleriben@hotmail.com>
2024-03-31 23:37:33 +0100

    Began creating page
    started to add elements to the page and style them according to figma design.

--- src/pages/userInfo/userInfo.tsx
+++ src/pages/userInfo/userInfo.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import styles from "./userInfo.module.css"
 import { Outlet } from 'react-router-dom'
-import Sidebar from '../../components/sidebar/Sidebar.tsx'
-import Navbar from '../../components/navbar/Navbar.tsx'
+import { FaPencilAlt } from "react-icons/fa"
 
 function userInfo() {
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
-                Stuff
+                <div className={styles.contact_panel}>
+                    <div className={styles.title}>
+                        <h1>Contact Information: </h1>
+                        <button className={styles.edit_btn}>< FaPencilAlt size={20}/></button>
+                    </div>
+                    <div className={styles.address}></div>
+                    <div className={styles.contact}></div>
+                    <div className={styles.emergency_contact}></div>
+                </div>
+                <div className={styles.personal_info_panel}>
+                <div className={styles.title}>
+                        <h1>Personal Information: </h1>
+                        <button className={styles.edit_btn}>< FaPencilAlt size={20}/></button>
+                    </div>
+                    <div className={styles.details}></div>
+                    <div className={styles.office}></div>
+                    <div className={styles.work_details}></div>
+                </div>
             </div>
         </div>
       )
