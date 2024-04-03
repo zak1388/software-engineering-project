5ffa730d1bbc7c2e133a6b833d321041f6ca05d2
Zakariyya Kadri <zakariyyakadri@gmail.com>
2024-04-03 23:57:08 +0100

    Made Issue a component

--- src/pages/AdminIssues/AdminIssues.tsx
+++ src/pages/AdminIssues/AdminIssues.tsx
@@ -12,63 +12,36 @@ function AdminIssues () {
         <div className={styles.container}>
             <div className={styles.IssuesBundle}>
                 <h1>Employee Issues</h1>
-                <div className={styles.SingleIssue}>
-                    <div className={styles.Name}>
-                        <h3>Employee Name</h3>
-                        <h1>Rochak Sharma</h1>
-                    </div>
-                    <div className={styles.Date}>
-                        <h3>Date Raised</h3>
-                        <h1>19th March 2024</h1>
-                    </div>
-
-                    <div className={styles.Brief}>
-                        <h3>Brief</h3>
-                        <h1>Cannot change emergency contact to a landline number.</h1> {/*should have a word limit when uploading*/}
-                    </div>
-
-                    {showModal && (
-                        <div className={styles.Modal}>
-                            <div className={styles.ModalContent}>
-                                <div className={styles.ModalHeading}>
-                                    <h1>Rochak Sharma (220563998)</h1>
-                                    <span className={styles.close} onClick={() => setShowModal(false)}>&times;</span>
-                                </div>
-                                <h2>Cannot change emergency contact to a landline number.</h2>
-                                <p>Inability to update emergency contacts to landline numbers poses a potential safety risk. Consider revising database settings to accommodate landline entries for comprehensive emergency preparedness and employee safety.</p>
-                            </div>
-                        </div>
-                    )}
-
-                    <div className={styles.Buttons}>
-                        <button className={styles.ReadMoreButton} onClick={() => handleReadMoreClick()}>Read More</button>
-                        <button className={styles.DeleteButton}>Delete</button>
-                    </div>
-
-                </div>
-                <div className={styles.SingleIssue}>
-                    <div className={styles.Name}>
-                        <h3>Employee Name</h3>
-                        <h1>Donald Trump</h1>
-                    </div>
-                    <div className={styles.Date}>
-                        <h3>Date Raised</h3>
-                        <h1>30th January 2024</h1>
-                    </div>
-
-                    <div className={styles.Brief}>
-                        <h3>Brief</h3>
-                        <h1>Cannot win an election with this thing.</h1> {/*should have a word limit when uploading*/}
-                    </div>
-
-                    <div className={styles.Buttons}>
-                        <button className={styles.ReadMoreButton}>Read More</button>
-                        <button className={styles.DeleteButton}>Delete</button>
-                    </div>
-                </div>
+                <Issue name="Rochak Sharma" raisedOn="19th March 2024" brief="Cannot change emergency contact to a landline number"/>
+                <Issue name="Donald Trump" raisedOn="30th January 2024" brief="Cannot win an election with this thing."/>
             </div>    
         </div>
     )
+}
+
+function Issue({name, raisedOn, brief, description}) {
+    return (
+        <div className={styles.SingleIssue}>
+            <div className={styles.Name}>
+                <h3>Employee Name</h3>
+                <h1>{name}</h1>
+            </div>
+            <div className={styles.Date}>
+                <h3>Date Raised</h3>
+                <h1>{raisedOn}</h1>
+            </div>
+
+            <div className={styles.Brief}>
+                <h3>Brief</h3>
+                <h1>{brief}</h1> {/*should have a word limit when uploading*/}
+            </div>
+
+            <div className={styles.Buttons}>
+                <button className={styles.ReadMoreButton}>Read More</button>
+                <button className={styles.DeleteButton}>Delete</button>
+            </div>
+        </div>
+    );
 }
 
 export default AdminIssues
