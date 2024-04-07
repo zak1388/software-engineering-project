f0158405d5ae14be3935e705f9dfca7f1c473188
Benjamin Camilleri <camilleriben@hotmail.com>
2024-04-07 21:07:55 +0100

    Made new display details editable

--- src/pages/dashboard/profile/EditProfile.tsx
+++ src/pages/dashboard/profile/EditProfile.tsx
@@ -10,7 +10,14 @@ interface userDetails{
     first_name: String,
     last_name: String,
     email: String,
-    address: String
+    address: String,
+    personal_number: String,
+    emergency_number: String,
+    holiday_days: Number,
+    office_location: String,
+    gender: String,
+    position: String,
+    date_of_birth: Date
 }
 
 
@@ -23,6 +30,8 @@ function Profile() {
     const [lastName, setLastName] = useState("")
     const [email, setEmail] = useState("")
     const [address, setAddress] = useState("")
+    const [personal_number, setPersonalNumber] = useState("")
+    const [emergency_number, setEmergencyNumber] = useState("")
 
     const nav = useNavigate()
 
@@ -46,7 +55,9 @@ function Profile() {
             first_name: firstName ? firstName : userDetails?.first_name,
             last_name: lastName ? lastName : userDetails?.last_name,
             email: email ? email : userDetails?.email,
-            address: address ? address : userDetails?.address
+            address: address ? address : userDetails?.address,
+            personal_number: personal_number ? personal_number : userDetails?.personal_number,
+            emergency_number: emergency_number ? emergency_number : userDetails?.emergency_number
         })
         nav(`/profile/${id}`)
     }
@@ -70,6 +81,8 @@ function Profile() {
                             <input type="text" placeholder={userDetails?.last_name}  value={lastName} onChange={((e) => setLastName(e.target.value))} />
                             <input type="text" placeholder={userDetails?.email} value={email} onChange={((e) => setEmail(e.target.value))} />
                             <input type="text" placeholder={userDetails?.address} value={address} onChange={((e) => setAddress(e.target.value))} />
+                            <input type="text" placeholder={userDetails?.personal_number} value={personal_number} onChange={((e) => setPersonalNumber(e.target.value))} />
+                            <input type="text" placeholder={userDetails?.emergency_number} value={emergency_number} onChange={((e) => setEmergencyNumber(e.target.value))} />
                             <div className={styles.button}>
                                 <button onClick={updateProfile}>Save</button>
                             </div>
